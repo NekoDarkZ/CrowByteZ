67fb90802df09dcc3607e827b3ac7da8e5bef7bc
Scrowel <nicolas.henriquez@alumnos.ucn.cl>
2018-04-19 17:00:40 -0300

    Game
    Prueba

---
+++ Assets/scripts/atacar.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class atacar : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Preguntar por una tecla para accionar algo cuando es presionada (En este caso la flecha derecha)
+
+		if (Input.GetKeyUp(KeyCode.Z))
+		{
+			GetComponent<Animator>().SetBool("atacar", false);
+		}
+		if (Input.GetKeyDown(KeyCode.Z))
+		{
+			GetComponent<Animator>().SetBool("atacar", true);
+		}
+	}
+		
+	}
+
