5005fe2beb313315a2faacf0ae2cb60bda620544
NekoDarkZ <sebastian.murquio@alumnos.ucn.cl>
2018-05-06 21:02:57 -0300

    menu holi

---
+++ Assets/escenas/Menu/ManagerNivel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ManagerNivel : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void CargarNivel(string pNombreNivel){
+		SceneManager.LoadScene(pNombreNivel);
+	}
+}
