e47f1db5e6698b3e3e99633439afcca3fa46eeb9
Maxur1 <marcelo.soto01@alumnos.ucn.cl>
2018-05-14 21:45:37 -0400

    pelea

---
+++ Assets/scripts/Enemigo.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemigo : MonoBehaviour {
+    [SerializeField] GameObject protagonist;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+    public void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.tag == "espada")
+        {
+            Destroy(gameObject);
+        }
+        if (col.gameObject.tag == "espadaD")
+        {
+
+            if (protagonist.GetComponent<SpriteRenderer>().flipX == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (col.gameObject.tag == "espadaI")
+        {
+            if (protagonist.GetComponent<SpriteRenderer>().flipX == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+    public void OnCollisionStay2D(Collision2D col)
+    {
+        if(col.gameObject.tag == "espada")
+        {
+
+                Destroy(gameObject);
+
+        }
+        if (col.gameObject.tag == "espadaD")
+        {
+    
+            if (protagonist.GetComponent<SpriteRenderer>().flipX == false)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (col.gameObject.tag == "espadaI")
+        {
+            if (protagonist.GetComponent<SpriteRenderer>().flipX == true)
+            {
+                Destroy(gameObject);
+            }
+        }
+        if (col.gameObject.tag == "Player")
+        {
+            Destroy(col.gameObject);
+        }
+    }
+}
--- Assets/scripts/Jugador.cs
+++ Assets/scripts/Jugador.cs
@@ -12,61 +12,62 @@ public class Jugador : MonoBehaviour {
 	}
 
 
-// Update is called once per frame
-void Update () {
-		////Codigo de movimiento////
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			//flipea la  imagen a la derecha si esta hacia la izquierda
-			if (GetComponent<SpriteRenderer>().flipX == true)
-			{
-				GetComponent<SpriteRenderer>().flipX = false;
-			}
-			//mueve el personaje a la derechachacha
-			GetComponent<Animator> ().SetBool ("caminar", true);
-			transform.Translate (velocidad, 0, 0);
-			
-		}
-		//Preguntar por una tecla para accionar algo cuando es presionada (En este caso la flecha izquierda)
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			//flipea la  imagen a la izquierda si esta hacia la derecha
-			if (GetComponent<SpriteRenderer>().flipX == false)
-			{
-				GetComponent<SpriteRenderer>().flipX = true;
-			}
-			//mueve el personaje a la izquierdadada
-			GetComponent<Animator> ().SetBool ("caminar", true);
-			transform.Translate (-velocidad, 0, 0);
-			
-		}
-		//esto es para eliminar el movimiento
-		if (Input.GetKeyUp(KeyCode.RightArrow))
-		{
-			GetComponent<Animator>().SetBool("caminar", false);
-		}
-		if (Input.GetKeyUp(KeyCode.LeftArrow))
-		{
-			GetComponent<Animator>().SetBool("caminar", false);
-		}
-		if (Input.GetKey(KeyCode.UpArrow))
-		{
+    // Update is called once per frame
+    void Update()
+    {
+        ////Codigo de movimiento////
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            //flipea la  imagen a la derecha si esta hacia la izquierda
+            if (GetComponent<SpriteRenderer>().flipX == true)
+            {
+                GetComponent<SpriteRenderer>().flipX = false;
+            }
+            //mueve el personaje a la derechachacha
+            GetComponent<Animator>().SetBool("caminar", true);
+            transform.Translate(velocidad, 0, 0);
 
-			//mueve el personaje a la izquierdadada
+        }
+        //Preguntar por una tecla para accionar algo cuando es presionada (En este caso la flecha izquierda)
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //flipea la  imagen a la izquierda si esta hacia la derecha
+            if (GetComponent<SpriteRenderer>().flipX == false)
+            {
+                GetComponent<SpriteRenderer>().flipX = true;
+            }
+            //mueve el personaje a la izquierdadada
+            GetComponent<Animator>().SetBool("caminar", true);
+            transform.Translate(-velocidad, 0, 0);
 
-			GetComponent<Animator> ().SetBool ("saltar", true);
-			transform.Translate (0, salto, 0);
+        }
+        //esto es para eliminar el movimiento
+        if (Input.GetKeyUp(KeyCode.RightArrow))
+        {
+            GetComponent<Animator>().SetBool("caminar", false);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftArrow))
+        {
+            GetComponent<Animator>().SetBool("caminar", false);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
 
-		}
-		if (!Input.GetKey(KeyCode.UpArrow))
-		{
+            //mueve el personaje a la izquierdadada
 
-			//mueve el personaje a la izquierdadada
+            GetComponent<Animator>().SetBool("saltar", true);
+            transform.Translate(0, salto, 0);
 
-			GetComponent<Animator> ().SetBool ("saltar", false);
-			transform.Translate (0, 0, 0);
-            
-		}
+        }
+        if (!Input.GetKey(KeyCode.UpArrow))
+        {
+
+            //mueve el personaje a la izquierdadada
+
+            GetComponent<Animator>().SetBool("saltar", false);
+            transform.Translate(0, 0, 0);
+
+        }
         ////codigo ofensivo////
 
         //ataque basico
@@ -90,5 +91,6 @@ void Update () {
         }
 
         ////codigo especial////
+
     }
-}
+   }
