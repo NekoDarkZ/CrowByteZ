345e61ea9d48483e46585d48a1e200d7a5194967
Scrowel <nicolas.henriquez@alumnos.ucn.cl>
2018-09-01 18:37:41 -0300

    jefe2unfinished
    jefe 2 40%

---
+++ Assets/Nicolas/Scripts/EstadoHechizo.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstadoHechizo : StateMachineBehaviour {
+    private float timerupdate;
+    private int random;
+    public float timer;
+    private Transform posicionjugador;
+    public GameObject prefab;
+    GameObject prefabClone;
+    public float tiempodestruccion;
+    private float timerfuego;
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        random = Random.Range(0, 2);
+        timerupdate = timer;
+        timerfuego= Time.time + 0.3f;
+    }
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (timerupdate <= 0)
+        {
+            if (random == 0)
+            {
+                animator.SetTrigger("movimiento");
+            }
+            else
+            {
+                animator.SetTrigger("pilar");
+            }
+        }
+        else
+        {
+            timerupdate -= Time.deltaTime;
+        }
+        if (Time.time > timerfuego)
+        {
+            posicionjugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+            prefabClone = Instantiate(prefab, new Vector2(posicionjugador.position.x, posicionjugador.position.y), Quaternion.identity) as GameObject;
+            Destroy(prefabClone, tiempodestruccion);
+            timerfuego += 0.3f;
+        }
+    }
+}
---
+++ Assets/Nicolas/Scripts/EstadoMovimiento.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EstadoMovimiento : StateMachineBehaviour {
+    private float timerupdate;
+    public float speed;
+    private int random;
+    public float timer;
+    private Transform posicionjugador;
+    
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        random = Random.Range(0, 2);
+        posicionjugador = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+        timerupdate = timer;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        if (timerupdate <= 0)
+        {
+
+            if (random==0)
+            {
+                animator.SetTrigger("pilar");
+            }
+            else
+            {
+                animator.SetTrigger("hechizo");
+            }
+        }
+        else
+        {
+            timerupdate -= Time.deltaTime;
+        }
+
+        Vector2 target = new Vector2(posicionjugador.position.x, animator.transform.position.y);
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);
+    }
+
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex){
+
+    }
+}
