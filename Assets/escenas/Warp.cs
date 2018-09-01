3c5d49eaf2dd308c6f8f389f263af20e6e22bbc8
NekoDarkZ <sebastian.murquio@alumnos.ucn.cl>
2018-09-01 20:23:01 -0300

    primera escena y minor bugs

---
+++ Assets/escenas/Warp.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Warp : MonoBehaviour {
+	
+	[SerializeField] Object warp;
+	[SerializeField] string nivel;
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "Warp") 
+		{
+			SceneManager.LoadScene(nivel);
+		}
+	}
+}
