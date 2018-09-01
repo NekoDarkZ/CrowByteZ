253701276549ec803ff7fd4cdff89e7ff9d3d17a
NekoDarkZ <sebastian.murquio@alumnos.ucn.cl>
2018-09-01 20:51:24 -0300

    canvas cargando

--- Assets/escenas/Warp/Warp.cs
+++ Assets/escenas/Warp/Warp.cs
@@ -5,13 +5,15 @@ using UnityEngine.SceneManagement;
 
 public class Warp : MonoBehaviour {
 	
-	[SerializeField] Object warp;
+	[SerializeField] GameObject warp;
 	[SerializeField] string nivel;
+	[SerializeField] GameObject Cargando;
 
 	void OnCollisionEnter2D (Collision2D collision)
 	{
-		if (collision.gameObject.tag == "Warp") 
+		if (collision.gameObject.tag == "Player") 
 		{
+			Cargando.SetActive (true);
 			SceneManager.LoadScene(nivel);
 		}
 	}
