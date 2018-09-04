231863447afd295b1f9d4d8fd5d1c87789ae722e
NekoDarkZ <sebastian.murquio@alumnos.ucn.cl>
2018-09-04 02:15:54 -0300

    creditos finales

---
+++ Assets/escenas/creditos finales/creditos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class creditos : MonoBehaviour {
+
+	[SerializeField] GameObject warp;
+	[SerializeField] string nivel;
+
+	void OnCollisionEnter2D (Collision2D collision)
+	{
+		if (collision.gameObject.tag == "creditos") 
+		{
+			SceneManager.LoadScene(nivel);
+		}
+	}
+}
